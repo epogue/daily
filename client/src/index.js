298bec2fb258b7aa4671c4eb27ce49ce52a8a1e3
Elliott Pogue <epogue@gmail.com>
2017-08-30 16:42:56 -0400

    Support HMR

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => {
+  return ReactDOM.render(<Component />, document.getElementById("root"));
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    var NextApp = require("./App").default;
+    render(NextApp);
+  });
+}
+
 registerServiceWorker();
